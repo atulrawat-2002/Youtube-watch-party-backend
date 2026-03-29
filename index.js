f7615bf38c97782bafc6d5372dfd550334c97805
Atul <dr6217811@gmail.com>
2026-03-29 12:09:02 +0530

    Still fixing ping request

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ setInterval(async () => {
     } catch (error) {
       console.log('Error in request interval', error.message);
     }
-}, 1000 * 60 * 10);
+}, 5000);
 
 app.use(express.json());
 app.use(morgan("dev"))
